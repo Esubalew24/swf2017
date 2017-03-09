b7d30258767a0491dd6909bb1687cbb90254090f
lavil <n3vila00@students.oamk.fi>
2017-03-09 17:02:07 +0200

    Mapview to view our map

--- mapview.py
+++ mapview.py
@@ -1,6 +1,8 @@
-from kivy.garden.mapview import MapView
 from kivy.app import App
+from kivy.garden.mapview import MapView
 
+
+# this is the map
 class MapViewApp(App):
     def build(self):
         mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
